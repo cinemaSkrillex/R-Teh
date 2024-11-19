e416c3c75b832b0221c24cd3260feae6f0601fdb
Alan Trebugeais <alan.trebugeais@epitech.eu>
2024-11-19 10:24:33 +0100

    [ADD] acceleration with a boolean true or false for air friction if needed

---
+++ bootstrap/Components/Acceleration.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Teh
+** File description:
+** Acceleration.hpp
+*/
+
+#pragma once
+
+struct Acceleration {
+    float ax;
+    float ay;
+    float az;
+    bool  air_friction;
+};
--- bootstrap/Systems/ControlSystem.hpp
+++ bootstrap/Systems/ControlSystem.hpp
@@ -10,6 +10,7 @@
 #include "Registry.hpp"
 #include "Velocity.hpp"
 #include "Controllable.hpp"
+#include "Acceleration.hpp"
 
 enum class Action { MoveUp, MoveDown, MoveLeft, MoveRight };
 
@@ -22,28 +23,40 @@ class ControlSystem {
         keyBindings[sf::Keyboard::A] = Action::MoveLeft;
         keyBindings[sf::Keyboard::D] = Action::MoveRight;
 
-        // action handlers are lambdas that take a velocity and modify it
-        actionHandlers[Action::MoveUp]   = [this](Velocity& velocity) { handleMoveUp(velocity); };
-        actionHandlers[Action::MoveDown] = [this](Velocity& velocity) { handleMoveDown(velocity); };
-        actionHandlers[Action::MoveLeft] = [this](Velocity& velocity) { handleMoveLeft(velocity); };
-        actionHandlers[Action::MoveRight] = [this](Velocity& velocity) {
-            handleMoveRight(velocity);
+        // Initialize action handlers
+        actionHandlers[Action::MoveUp] = [this](Velocity& velocity, Acceleration& acceleration) {
+            handleMoveUp(velocity, acceleration);
+        };
+        actionHandlers[Action::MoveDown] = [this](Velocity& velocity, Acceleration& acceleration) {
+            handleMoveDown(velocity, acceleration);
+        };
+        actionHandlers[Action::MoveLeft] = [this](Velocity& velocity, Acceleration& acceleration) {
+            handleMoveLeft(velocity, acceleration);
+        };
+        actionHandlers[Action::MoveRight] = [this](Velocity& velocity, Acceleration& acceleration) {
+            handleMoveRight(velocity, acceleration);
         };
     }
 
     void update(Registry& registry) {
         auto& velocities    = registry.get_components<Velocity>();
         auto& controllables = registry.get_components<Controllable>();
+        auto& accelerations = registry.get_components<Acceleration>();
 
         for (std::size_t i = 0; i < controllables.size(); ++i) {
-            if (controllables[i] && velocities[i]) {
-                velocities[i]->vx = 0.0f;
-                velocities[i]->vy = 0.0f;
-
+            if (controllables[i] && velocities[i] && accelerations[i]) {
+                bool keyPressed = false;
                 for (const auto& [key, action] : keyBindings) {
                     if (sf::Keyboard::isKeyPressed(key)) {
-                        actionHandlers[action](*velocities[i]);
+                        keyPressed = true;
+                        actionHandlers[action](*velocities[i], *accelerations[i]);
                     }
+                }
+                if (!keyPressed && accelerations[i]->air_friction == true) {
+                    applyDeceleration(*velocities[i], *accelerations[i]);
+                } else if (!keyPressed && accelerations[i]->air_friction == false) {
+                    velocities[i]->vx = 0;
+                    velocities[i]->vy = 0;
                 }
             }
         }
@@ -52,14 +65,44 @@ class ControlSystem {
     void bindKey(sf::Keyboard::Key key, Action action) { keyBindings[key] = action; }
 
   private:
-    std::unordered_map<sf::Keyboard::Key, Action>              keyBindings;
-    std::unordered_map<Action, std::function<void(Velocity&)>> actionHandlers;
+    std::unordered_map<sf::Keyboard::Key, Action>                             keyBindings;
+    std::unordered_map<Action, std::function<void(Velocity&, Acceleration&)>> actionHandlers;
 
-    void handleMoveUp(Velocity& velocity) { velocity.vy = -10.0f; }
+    void handleMoveUp(Velocity& velocity, Acceleration& acceleration) {
+        velocity.vy -= acceleration.ay;
+    }
 
-    void handleMoveDown(Velocity& velocity) { velocity.vy = 10.0f; }
+    void handleMoveDown(Velocity& velocity, Acceleration& acceleration) {
+        velocity.vy += acceleration.ay;
+    }
 
-    void handleMoveLeft(Velocity& velocity) { velocity.vx = -10.0f; }
+    void handleMoveLeft(Velocity& velocity, Acceleration& acceleration) {
+        velocity.vx -= acceleration.ax;
+    }
 
-    void handleMoveRight(Velocity& velocity) { velocity.vx = 10.0f; }
+    void handleMoveRight(Velocity& velocity, Acceleration& acceleration) {
+        velocity.vx += acceleration.ax;
+    }
+
+    void applyDeceleration(Velocity& velocity, Acceleration& acceleration) {
+        if (velocity.vx > 0) {
+            velocity.vx -= acceleration.ax;
+            if (velocity.vx < 0)
+                velocity.vx = 0;
+        } else if (velocity.vx < 0) {
+            velocity.vx += acceleration.ax;
+            if (velocity.vx > 0)
+                velocity.vx = 0;
+        }
+
+        if (velocity.vy > 0) {
+            velocity.vy -= acceleration.ay;
+            if (velocity.vy < 0)
+                velocity.vy = 0;
+        } else if (velocity.vy < 0) {
+            velocity.vy += acceleration.ay;
+            if (velocity.vy > 0)
+                velocity.vy = 0;
+        }
+    }
 };
