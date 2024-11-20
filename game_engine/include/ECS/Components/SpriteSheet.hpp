b4f2ffd5eb0b6dca85da58110ab23337f2ffefd8
NoxLoveYa <ewan.massonet@epitech.eu>
2024-11-20 11:55:17 +0100

    [ADD] :sparkles: both components to customize Drawable

---
+++ game_engine/include/ECS/Components/SpriteSheet.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Teh
+** File description:
+** SpriteSheet
+*/
+
+#pragma once
+
+struct SpriteSheet {
+    float width, height; // Size of a single frame in the spritesheet.
+    float animTime; // Time in milliseconds before updating the frame.
+};
