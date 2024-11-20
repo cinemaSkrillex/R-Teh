4dffdf2959ba76f316a5c0ffd86140553ff4d349
Alan Trebugeais <alan.trebugeais@epitech.eu>
2024-11-20 22:37:02 +0100

    [ADD] :sparkles: dynamic library for linux and windows, need to test on windows

--- network/main.cpp
+++ network/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <dlfcn.h>
 #include <asio.hpp>
+#include "include/DynamicLibrary/DynamicLIbrary.hpp"
 
 typedef void (*UDPServerConstructor)(asio::io_context&, unsigned short);
 
@@ -18,30 +19,29 @@ int main(int argc, char* argv[]) {
 
     unsigned short port = static_cast<unsigned short>(std::stoi(argv[1]));
 
-    void* handle = dlopen("librtype_server.so", RTLD_LAZY);
-    if (!handle) {
-        std::cerr << "Cannot open library: " << dlerror() << std::endl;
-        return 1;
-    }
-
-    dlerror(); // Reset errors
-
-    UDPServerConstructor create_server = (UDPServerConstructor)dlsym(handle, "create_udp_server");
-    const char*          dlsym_error   = dlerror();
-    if (dlsym_error) {
-        std::cerr << "Cannot load symbol 'create_udp_server': " << dlsym_error << std::endl;
-        dlclose(handle);
-        return 1;
-    }
-
     try {
-        asio::io_context io_context;
-        create_server(io_context, port);
+        std::string library_extension;
+#if defined(_WIN32) || defined(_WIN64)
+        library_extension = ".dll";
+#else
+        library_extension = ".so";
+#endif
+
+        // Construct the library file name
+        std::string    library_name = "librtype_server" + library_extension;
+        DynamicLibrary lib(library_name);
+        auto           create_server =
+            lib.get_symbol<UDPServerConstructor* (*)(asio::io_context&, unsigned short)>(
+                "create_udp_server");
+
+        asio::io_context      io_context;
+        UDPServerConstructor* server = create_server(io_context, port);
         io_context.run();
-    } catch (std::exception& e) {
+        delete server; // Clean up the server instance
+    } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
+        return 1;
     }
 
-    dlclose(handle);
     return 0;
 }
