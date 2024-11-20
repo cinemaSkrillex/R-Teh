0154256354aa26f075b7de2b204fe9f930fe0889
Quentin Duval <quentin1.duval@epitech.eu>
2024-11-20 11:55:49 +0100

    [ADD] interface classes for medias

---
+++ game_engine/include/Media/Graphics/Render.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <cmath>
+
+namespace engine {
+class IRender {
+  public:
+    IRender()                                  = default;
+    ~IRender()                                 = default;
+    virtual void disappear()                   = 0;
+    virtual void setPosition(float x, float y) = 0;
+    virtual void setScale(float x, float y)    = 0;
+    virtual void setRotation(float angle)      = 0;
+};
+} // namespace engine
