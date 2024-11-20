700ddf6ab3f373bacf3af241c65d1eeafd9be4a6
NoxLoveYa <ewan.massonet@epitech.eu>
2024-11-20 17:01:10 +0100

    [ADD] :sparkles: Prelude of sprite component working in drawable.

--- game_engine/include/ECS/Components/SpriteSheet.hpp
+++ game_engine/include/ECS/Components/SpriteSheet.hpp
@@ -7,7 +7,10 @@
 
 #pragma once
 
+#include "Sprite.hpp"
+
 struct SpriteSheet {
+    Sprite sprite;
     float width, height; // Size of a single frame in the spritesheet.
     float animTime; // Time in milliseconds before updating the frame.
 };
--- game_engine/src/ECS/Systems/DrawSystem.cpp
+++ game_engine/src/ECS/Systems/DrawSystem.cpp
@@ -9,13 +9,24 @@
 
 DrawSystem::DrawSystem(sf::RenderWindow& window) : _window(window) {}
 
-void DrawSystem::update(Registry& registry, SparseArray<Position>& positions,
-                        SparseArray<Drawable>& drawables, float deltaTime) {
+void DrawSystem::update(Registry& registry, float deltaTime, SparseArray<Position>& positions,
+                        SparseArray<Drawable>& drawables, SparseArray<Sprite>& sprites) {
     for (std::size_t i = 0; i < drawables.size(); ++i) {
-        if (drawables[i] && positions[i]) {
+        if (!drawables[i])
+            continue;
+
+        if (!positions[i])
+            continue;
+
+        if (sprites[i]) {
             sf::RectangleShape shape(sf::Vector2f(50.0f, 50.0f));
             shape.setPosition(positions[i]->x, positions[i]->y);
             shape.setFillColor(sf::Color::Green);
+            _window.draw(shape);
+        } else {
+            sf::RectangleShape shape(sf::Vector2f(50.0f, 50.0f));
+            shape.setPosition(positions[i]->x, positions[i]->y);
+            shape.setFillColor(sf::Color::Blue);
             _window.draw(shape);
         }
     }
