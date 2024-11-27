567894d4a1c2a26aaaa6ae8ed7a455a691dd2df6
Thibaut Tavernier <thibaut1.tavernier@epitech.eu>
2024-11-27 15:32:35 +0100

    [FIX] :bug: reorganize collision function

--- rtype_game/main.cpp
+++ rtype_game/main.cpp
@@ -13,29 +13,8 @@
 #include "ECS/Systems/CollisionSystem.hpp"
 #include "Core/Window.hpp"
 #include "Controls.hpp"
+#include "CollisionHandler.hpp"
 
-
-void handle_collision(Registry& registry, const std::unordered_map<std::string, Entity>& entities) {
-    auto& collisions = registry.get_components<Collision>();
-    auto& positions = registry.get_components<Position>();
-
-    Entity spaceship2 = entities.at("spaceship2");
-    Entity sol = entities.at("ground");
-
-    auto& spaceship2_collision = collisions[spaceship2];
-    auto& spaceship2_position = positions[spaceship2];
-    auto& sol_collision = collisions[sol];
-    auto& sol_position = positions[sol];
-
-
-    if (spaceship2_collision && sol_collision) {
-        if (spaceship2_collision->bounds.intersects(sol_collision->bounds)) {
-            if (spaceship2_position->y + spaceship2_collision->bounds.height > sol_position->y) {
-                spaceship2_position->y = sol_position->y - spaceship2_collision->bounds.height;
-            }
-        }
-    }
-}
 
 int main() {
     RealEngine::Window window("SKRILLEX", sf::Vector2u(800, 600));
