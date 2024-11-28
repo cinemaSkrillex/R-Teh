bb4e54d107bfd5eae461d7b532f2d88283d1272c
Quentin Duval <quentin1.duval@epitech.eu>
2024-11-28 13:50:21 +0100

    [FIX] boss spritesheet + boss animated

--- rtype_game/src/EyeBoss.cpp
+++ rtype_game/src/EyeBoss.cpp
@@ -5,9 +5,9 @@ EyeBoss::EyeBoss(RealEngine::Registry& registry)
     : _state(EyeBossState::SHORT_RANGE),
       _registry(registry),
       _entity(registry.spawn_entity()),
-      _shortSprite("../assets/sprites/the_eye/boss.png", sf::IntRect{0, 0, 55, 68}),
-      _midSprite("../assets/sprites/the_eye/boss.png", sf::IntRect{55, 0, 55, 91}),
-      _longSprite("../assets/sprites/the_eye/boss.png", sf::IntRect{55 * 2, 0, 55, 81}) {
+      _shortSprite("../assets/sprites/the_eye/boss.png", sf::IntRect{0, 0, 55 * 3, 73}),
+      _midSprite("../assets/sprites/the_eye/boss.png", sf::IntRect{0, 73, 55 * 3, 91}),
+      _longSprite("../assets/sprites/the_eye/boss.png", sf::IntRect{0, 73 + 91, 55 * 3, 81}) {
     _shortSprite.setScale(3, 3);
     _midSprite.setScale(3, 3);
     _longSprite.setScale(3, 3);
@@ -16,11 +16,11 @@ EyeBoss::EyeBoss(RealEngine::Registry& registry)
     _bossSheet.emplace("long", _longSprite);
     // _registry.add_components(_entity, RealEngine::Position{100.0f, 100.0f});
     // _registry.add_component(_entity, RealEngine::Drawable{});
-    // // _registry.add_component(
-    // //     _entity, RealEngine::SpriteSheet{_bossSheet, "idle", 0, {32, 15}, false, false, 100});
     // _registry.add_component(_entity, RealEngine::SpriteComponent{_shortSprite});
-    _registry.add_components(_entity, RealEngine::Position{500.f, 500.f}, RealEngine::Drawable{});
-    _registry.add_component(_entity, RealEngine::SpriteComponent{_shortSprite});
+    _registry.add_components(_entity, RealEngine::Position{200.f, 200.f}, RealEngine::Drawable{});
+    _registry.add_component(
+        _entity, RealEngine::SpriteSheet{_bossSheet, "short", 0, {55, 73}, false, true, 120});
+    // _registry.add_component(_entity, RealEngine::SpriteComponent{_shortSprite});
     _registry.add_component(_entity, RealEngine::Rotation{300.0f});
 }
 
