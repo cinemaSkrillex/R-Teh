4d7fc6e9a26089cc6ca64195bb75c05d0c35f64c
Alan Trebugeais <alan.trebugeais@epitech.eu>
2024-11-30 13:05:42 +0100

    [FIX] :bug: files not incldued for windows

--- game_engine/include/ECS/Components/Radius.hpp
+++ game_engine/include/ECS/Components/Radius.hpp
@@ -7,14 +7,14 @@
 
 namespace RealEngine {
 
-enum class RadiusState { NONE, IN, OUTER_MAX, OUTER_MIN };
+enum class RadiusState { NONE = 0, IN = 1, OUTER_MAX = 2, OUTER_MIN = 3 };
 
 struct Radius {
-    float                 sizeMax;
-    float                 sizeMin;
-    std::function<void()> behaviorIn       = []() {};
-    std::function<void()> behaviorOuterMax = []() {};
-    std::function<void()> behaviorOuterMin = []() {};
-    RadiusState           lastState        = RadiusState::NONE;
+    float                   sizeMax;
+    float                   sizeMin;
+    std::function<void()>   behaviorIn       = []() {};
+    std::function<void()>   behaviorOuterMax = []() {};
+    std::function<void()>   behaviorOuterMin = []() {};
+    RealEngine::RadiusState lastState        = RealEngine::RadiusState::NONE;
 };
 }  // namespace RealEngine
