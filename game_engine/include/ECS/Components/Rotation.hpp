042f769f9bcae82429cf771603bda75bb2ab484c
Alan Trebugeais <alan.trebugeais@epitech.eu>
2024-11-30 12:20:47 +0100

    [ADD] msvc version 17

--- game_engine/include/ECS/Components/Rotation.hpp
+++ game_engine/include/ECS/Components/Rotation.hpp
@@ -1,7 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Teh
+** File description:
+** Rotation.hpp
+*/
+
 #pragma once
 
 namespace RealEngine {
 struct Rotation {
     float angle;
 };
-} // namespace RealEngine
+}  // namespace RealEngine
