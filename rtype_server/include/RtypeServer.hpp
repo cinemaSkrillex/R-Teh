07fbeab56545f87fa96866480492375c2354e98a
NoxLoveYa <ewan.massonet@epitech.eu>
2024-12-11 22:18:28 +0100

    [FIX] :clown_face:

--- rtype_server/include/RtypeServer.hpp
+++ rtype_server/include/RtypeServer.hpp
@@ -30,7 +30,7 @@
 #include <windows.h>
 #endif
 
-const sf::Int32    SERVER_TICK           = 10;
+const sf::Int32    SERVER_TICK           = 30;
 const sf::Int32    SERVER_BROADCAST_TICK = 10;
 const sf::Vector2f PLAYER_START_POSITION = {50.f, 250.f};
 const float        SNAP_TRESHOLD         = 3.5f;
