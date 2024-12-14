6c75600b61ba50dfb454c6b0f7be0259d403be72
Quentin D <quentin1.duval@epitech.eu>
2024-12-14 22:20:10 +0000

    [FIX] long int

--- rtype_server/src/RtypeServer/RtypeServerCallbacks.cpp
+++ rtype_server/src/RtypeServer/RtypeServerCallbacks.cpp
@@ -43,13 +43,13 @@ void RtypeServer::initCallbacks() {
         long elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(
                                 std::chrono::steady_clock::now() - _startTime)
                                 .count();
-        auto player      = Player(uuid, elapsed_time,
-                                  _game_instance->addAndGetPlayer(uuid, _config.PLAYER_START_POSITION),
-                                  _game_instance->getRegistry());
-        _players[sender] = player;
-        int entity_uuid  = uuid_generator.generate_long();
-        // _server_entities[entity_uuid] =
-        // _game_instance->addAndGetEntity({500.f, 200.f});
+        auto player          = Player(uuid, elapsed_time,
+                                      _game_instance->addAndGetPlayer(uuid, _config.PLAYER_START_POSITION),
+                                      _game_instance->getRegistry());
+        _players[sender]     = player;
+        long int entity_uuid = uuid_generator.generate_long();
+        _server_entities[entity_uuid] =
+            _game_instance->addAndGetEntity(entity_uuid, {500.f, 200.f});
         std::cout << "Added entity with uuid: " << entity_uuid << std::endl;
     });
 }
