61aa6a3cb63db222995725df2c8f2e5646a7758c
NoxLoveYa <ewan.massonet@epitech.eu>
2024-12-14 20:13:54 +0100

    [FIX] :clown_face:

--- rtype_server/main.cpp
+++ rtype_server/main.cpp
@@ -15,11 +15,18 @@ int main(int argc, char* argv[]) {
 
     unsigned short port = static_cast<unsigned short>(std::stoi(argv[1]));
 
+    std::cout << "Starting server on port " << port << std::endl;
+
     try {
         asio::io_context             io_context;
+        std::cout << "IO context created" << std::endl;
         auto                         server       = std::make_shared<UDPServer>(io_context, port);
+        std::cout << "Server created" << std::endl;
         std::shared_ptr<RtypeServer> rtype_server = std::make_shared<RtypeServer>(server);
+        std::cout << "RtypeServer created" << std::endl;
         std::chrono::steady_clock::time_point start_time = rtype_server->getStartTime();
+
+        std::cout << "Server started at " << start_time.time_since_epoch().count() << std::endl;
 
         rtype_server->run();
 
