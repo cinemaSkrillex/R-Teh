eb6ad1a0ce694968c14119699c156365dd841680
Quentin D <quentin1.duval@epitech.eu>
2025-01-02 23:24:26 +0000

    [FIX] crash when player have no sprite because the acess to sprite component is compromised

--- rtype_game/src/Game/Game.cpp
+++ rtype_game/src/Game/Game.cpp
@@ -35,8 +35,9 @@ std::shared_ptr<RealEngine::Entity> Game::add_mob(long int enemy_uuid, sf::Vecto
 
 sf::IntRect Game::getPlayerNormalizedDirection() {
     sf::IntRect direction(0, 0, 0, 0);
+    auto*       player_sprite = _registry.get_component<RealEngine::SpriteSheet>(_player_entity);
 
-    if (_window.isFocused() && _player_entity != nullptr) {
+    if (player_sprite && _window.isFocused()) {
         if (_controlSystem.isActionPressed(RealEngine::Action::Left)) {
             direction.left = 1;
         }
@@ -44,15 +45,15 @@ sf::IntRect Game::getPlayerNormalizedDirection() {
             direction.top = 1;
         }
         if (_controlSystem.isActionPressed(RealEngine::Action::Up)) {
-            direction.width                                                               = 1;
-            _registry.get_component<RealEngine::SpriteSheet>(_player_entity)->spriteIndex = "up";
+            direction.width            = 1;
+            player_sprite->spriteIndex = "up";
         }
         if (_controlSystem.isActionPressed(RealEngine::Action::Down)) {
-            direction.height                                                              = 1;
-            _registry.get_component<RealEngine::SpriteSheet>(_player_entity)->spriteIndex = "down";
+            direction.height           = 1;
+            player_sprite->spriteIndex = "down";
         }
         if (direction.width == direction.height) {
-            _registry.get_component<RealEngine::SpriteSheet>(_player_entity)->spriteIndex = "idle";
+            player_sprite->spriteIndex = "idle";
         }
     }
     return direction;
