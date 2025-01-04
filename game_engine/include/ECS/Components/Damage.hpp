5523c8fefd08519be8e7267efc1c18523bf6d447
Quentin D <quentin1.duval@epitech.eu>
2025-01-04 22:13:26 +0000

    [ADD] explanations on health systems

--- game_engine/include/ECS/Components/Damage.hpp
+++ game_engine/include/ECS/Components/Damage.hpp
@@ -6,9 +6,9 @@ namespace RealEngine {
 
 struct Damage {
     float amount;
-    bool  effect         = false;
-    float effectDuration = 0;
-    float effectInterval = 0;
-    bool  stealLife      = false;
+    bool  effect         = false;  // damage during time
+    float effectDuration = 0;      // duration of the effect
+    float effectInterval = 0;      // interval between each damage
+    bool  stealLife      = false;  // each damage taken give tha half of the amount to the attacker
 };
 }  // namespace RealEngine
