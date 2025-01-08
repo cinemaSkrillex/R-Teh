b6cfeb55b118f7753185325dd91cdf19bedd3fcb
Alan Trebugeais <alan.trebugeais@epitech.eu>
2025-01-08 18:17:59 +0100

    [DEL] :x: unused comments

--- network/src/Server/UDPServer.cpp
+++ network/src/Server/UDPServer.cpp
@@ -17,9 +17,7 @@
 #include "../../include/Server/UDPServer.hpp"
 
 UDPServer::UDPServer(asio::io_context& io_context, unsigned short port)
-    : ANetwork<800>(io_context, port, Role::SERVER) {
-    // You can add additional constructor logic here
-}
+    : ANetwork<800>(io_context, port, Role::SERVER) {}
 
 void UDPServer::setEndpoint(const asio::ip::udp::endpoint& endpoint) {
     _client_endpoint = endpoint;
@@ -30,44 +28,42 @@ const asio::ip::udp::endpoint& UDPServer::getEndpoint() const { return _client_e
 // Callbacks
 void UDPServer::setNewClientCallback(
     const std::function<void(const asio::ip::udp::endpoint&)>& callback) {
-    ANetwork<800>::setNewClientCallback(callback);  // Call the inherited method
+    ANetwork<800>::setNewClientCallback(callback);
 }
 
 // Send methods
 void UDPServer::send_unreliable_packet(const std::array<char, 800>&   message,
                                        const asio::ip::udp::endpoint& endpoint) {
-    ANetwork<800>::send_unreliable_packet(message, endpoint);  // Call the inherited method
+    ANetwork<800>::send_unreliable_packet(message, endpoint);
 }
 
 void UDPServer::send_reliable_packet(const std::array<char, 800>&   message,
                                      const asio::ip::udp::endpoint& endpoint) {
-    ANetwork<800>::send_reliable_packet(message, endpoint);  // Call the inherited method
+    ANetwork<800>::send_reliable_packet(message, endpoint);
 }
 
 // Receive methods
 std::vector<std::array<char, 800>> UDPServer::get_unreliable_messages_from_endpoint(
     const asio::ip::udp::endpoint& endpoint) {
-    return ANetwork<800>::get_unreliable_messages_from_endpoint(
-        endpoint);  // Call the inherited method
+    return ANetwork<800>::get_unreliable_messages_from_endpoint(endpoint);
 }
 
 std::vector<std::array<char, 800>> UDPServer::get_reliable_messages_from_endpoint(
     const asio::ip::udp::endpoint& endpoint) {
-    return ANetwork<800>::get_reliable_messages_from_endpoint(
-        endpoint);  // Call the inherited method
+    return ANetwork<800>::get_reliable_messages_from_endpoint(endpoint);
 }
 
 std::array<char, 800> UDPServer::get_last_unreliable_packet_data() {
-    return ANetwork<800>::get_last_unreliable_packet_data();  // Call the inherited method
+    return ANetwork<800>::get_last_unreliable_packet_data();
 }
 
 std::array<char, 800> UDPServer::get_last_reliable_packet_data() {
-    return ANetwork<800>::get_last_reliable_packet_data();  // Call the inherited method
+    return ANetwork<800>::get_last_reliable_packet_data();
 }
 
 // Miscellaneous methods
 void UDPServer::send_new_client(const asio::ip::udp::endpoint& endpoint) {
-    ANetwork<800>::send_new_client(endpoint);  // Call the inherited method
+    ANetwork<800>::send_new_client(endpoint);
 }
 
 std::unordered_set<asio::ip::udp::endpoint, EndpointHash, EndpointEqual> UDPServer::getClients() {
