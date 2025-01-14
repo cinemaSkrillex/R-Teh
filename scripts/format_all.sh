0c597b3d2c42788fac31c5c7c0e1f88099c328c4
Thibaut Tavernier <thibaut1.tavernier@epitech.eu>
2025-01-14 10:33:32 +0100

    [FIX] :bug: format_all.sh with all directory

--- scripts/format_all.sh
+++ scripts/format_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find game_engine/include game_engine/src rtype_server/include rtype_server/src rtype_game/include rtype_game/src network/include network/src -type f \( -name "*.hpp" -o -name "*.cpp" \) | xargs clang-format -i
+find game_engine/include game_engine/src rtype_server/include rtype_server/src rtype_game/include rtype_game/src network/include network/src rtype_launcher_game/include rtype_launcher_game/src rtype_launcher_server/include rtype_launcher_server/src rtype_pong/include rtype_pong/src rtype_pong_server/include rtype_pong_server/src -type f \( -name "*.hpp" -o -name "*.cpp" \) | xargs clang-format -i
