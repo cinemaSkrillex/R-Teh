e04203f9d030100e44287907cf4468af4fde036e
Alan Trebugeais <alan.trebugeais@epitech.eu>
2025-01-17 20:48:05 +0100

    [ADÐ] :sparkles: missing files in launcher

--- rtype_launcher_server/include/Core/Audio/Sound.hpp
+++ rtype_launcher_server/include/Core/Audio/Sound.hpp
@@ -6,20 +6,17 @@ namespace RealEngine {
 class Sound : public IAudio {
    public:
     Sound(const std::string filePath);
-    Sound();
     ~Sound();
-    void            play() override;
-    void            stop() override;
-    bool            loadFile(const std::string filePath) override;
-    void            setVolume(float volume) override;
-    void            setPitch(float pitch) override;
-    sf::Sound       getSound() { return _sound; }
-    sf::SoundBuffer getBuffer() { return _buffer; }
+    void play() override;
+    void stop() override;
+    void loadFile(const std::string filePath) override;
+    void setVolume(float volume) override;
+    void setPitch(float pitch) override;
 
    private:
     sf::Sound       _sound;
     sf::SoundBuffer _buffer;
     float           _volume;
-    bool            _isPlaying = false;
+    bool            _isPlaying;
 };
 }  // namespace RealEngine
